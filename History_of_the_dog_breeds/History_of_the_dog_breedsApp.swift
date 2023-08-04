b97155986b424120e148f50602f1f2f68268493b
dat <s3651550@rmit.edu.vn>
2023-08-04 10:34:27 +0700

    Initial Commit

---
+++ History_of_the_dog_breeds/History_of_the_dog_breedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  History_of_the_dog_breedsApp.swift
+//  History_of_the_dog_breeds
+//
+//  Created by Dat, Tran Xuan Hoang on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct History_of_the_dog_breedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
